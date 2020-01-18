f6377c4db7e1f28776c3684366e920b39096722c
Timothy Sharkey <Techne3@gmail.com>
2020-01-18 11:06:29 -0700

    added react router and set up routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 import LandingPage from './LandingPage/LandingPage';
+import {Route, Switch, Link} from 'react-router-dom'
+import Search from './LandingPage/Search/Search'
 
 function App() {
   return (
-    <LandingPage />
+    <Switch>
+      <Route path='/search' component={Search}/>
+      <Route path='/' component={LandingPage}/>
+    </Switch>
   );
 }
 
